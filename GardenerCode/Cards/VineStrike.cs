295ad0bb193495386099e1b43fb321ff30a531ea
Mgns <you@example.com>
2026-05-19 22:13:15 -0500

    setup vine strike

---
+++ GardenerCode/Cards/VineStrike.cs
@@ -0,0 +1,27 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace Gardener;
+
+public class VineStrike() : GardenerCode.Cards.GardenerCard(
+  1,
+  CardType.Attack,
+  CardRarity.Basic,
+  TargetType.Self)
+{
+    protected override IEnumerable<MegaCrit.Sts2.Core.Localization.DynamicVars.DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+      ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+      await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+      .WithHitFx("vfx/vfx_attack_slash")
+      .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+      base.DynamicVars.Damage.UpgradeValueBy(4m);
+    }
+}
