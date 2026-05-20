d2b8a6d644bcdcaa9213c7713ff68b8d1257dc3b
Mgns <you@example.com>
2026-05-19 21:52:12 -0500

    rename "the gardener" to "gardener"

--- TheGardenerCode/Potions/TheGardenerPotion.cs
+++
@@ -1,8 +0,0 @@
-﻿using BaseLib.Abstracts;
-using BaseLib.Utils;
-using TheGardener.TheGardenerCode.Character;
-
-namespace TheGardener.TheGardenerCode.Potions;
-
-[Pool(typeof(TheGardenerPotionPool))]
-public abstract class TheGardenerPotion : CustomPotionModel;
