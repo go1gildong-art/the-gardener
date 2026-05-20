4dadfb8a0f8938faf1664bed2adf02827cd94b9a
Mgns <you@example.com>
2026-05-19 19:47:37 -0500

    clear files to fix template issue

--- the-gardenerCode/Potions/the-gardenerPotion.cs
+++
@@ -1,8 +0,0 @@
-﻿using BaseLib.Abstracts;
-using BaseLib.Utils;
-using the_gardener.the_gardenerCode.Character;
-
-namespace the_gardener.the_gardenerCode.Potions;
-
-[Pool(typeof(the_gardenerPotionPool))]
-public abstract class the_gardenerPotion : CustomPotionModel;
