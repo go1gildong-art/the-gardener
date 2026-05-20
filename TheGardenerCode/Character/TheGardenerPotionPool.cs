b449e631265c0a22e8cadcc0073c82f6a6604220
Mgns <you@example.com>
2026-05-19 20:55:25 -0500

    init template 2nd

---
+++ TheGardenerCode/Character/TheGardenerPotionPool.cs
@@ -0,0 +1,14 @@
+﻿using BaseLib.Abstracts;
+using TheGardener.TheGardenerCode.Extensions;
+using Godot;
+
+namespace TheGardener.TheGardenerCode.Character;
+
+public class TheGardenerPotionPool : CustomPotionPoolModel
+{
+    public override Color LabOutlineColor => TheGardener.Color;
+    
+
+    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+}
