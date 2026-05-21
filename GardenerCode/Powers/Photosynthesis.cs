af037510a178c91900c6aaf9f5096edfaad15742
Mgns <you@example.com>
2026-05-20 20:37:47 -0500

    add skeleton for photosyn power class

---
+++ GardenerCode/Powers/Photosynthesis.cs
@@ -0,0 +1,36 @@
+using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using Godot;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using BaseLib.Abstracts;
+
+namespace Gardener.GardenerCode.Powers;
+  
+
+  
+public class Photosynthesis : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override string CustomPackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "goldeqpower.png".PowerImagePath();
+        }
+    }
+
+    public override string CustomBigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "goldeqpower.png".BigPowerImagePath();
+        }
+    }
+}
