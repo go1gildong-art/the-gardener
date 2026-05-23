466d9ead4d9b995e350a071386d95db3ad435ea8
Mgns <you@example.com>
2026-05-23 12:42:11 -0500

    add leaf shield to starting deck

--- GardenerCode/Character/Gardener.cs
+++ GardenerCode/Character/Gardener.cs
@@ -32,7 +32,8 @@ public class Gardener : PlaceholderCharacterModel
         ModelDb.Card<DefendIronclad>(),
         ModelDb.Card<DefendIronclad>(),
         ModelDb.Card<VineStrike>(),
-        ModelDb.Card<Photosynthesis>()
+        ModelDb.Card<Photosynthesis>(),
+        ModelDb.Card<LeafShield>()
     ];
 
     public override IReadOnlyList<RelicModel> StartingRelics =>
