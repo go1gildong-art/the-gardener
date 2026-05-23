e64d42fc5c8a1fd5bbe3d967de7ec9447b2b380c
Mgns <you@example.com>
2026-05-23 12:27:59 -0500

    add leaf shield

---
+++ GardenerCode/Cards/LeafShield.cs
@@ -0,0 +1,56 @@
+
+namespace Gardener;
+
+using BaseLib.Utils;
+using Gardener.GardenerCode.Character;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+[Pool(typeof(GardenerCardPool))]
+public class LeafShield() : GardenerCode.Cards.GardenerCard(
+  0,
+  CardType.Skill,
+  CardRarity.Uncommon,
+  TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+{ new BlockVar(3m, BlockProps.card) };
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block, cardPlay);
+    }
+
+    public override async Task BeforeHandDraw(Player player, PlayerChoiceContext choiceContext, ICombatState combatState)
+    {
+        if (
+            player == base.Owner
+            && CombatManager.Instance.History.CardPlaysFinished.Any(
+                (CardPlayFinishedEntry e) => e.HappenedLastPlayerTurn(base.Owner) && e.CardPlay.Card == this
+                )
+                )
+        {
+            CardPile? pile = base.Pile;
+            if (pile == null || pile.Type != PileType.Hand)
+            {
+                await CardPileCmd.Add(this, PileType.Hand);
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+    }
+}
