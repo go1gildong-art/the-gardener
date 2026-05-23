498b1c49277b5a984cc3094cffbcf17d31431fb0
Mgns <you@example.com>
2026-05-23 13:05:12 -0500

    make skeleton for sharp roots

---
+++ GardenerCode/Cards/SharpRoots.cs
@@ -0,0 +1,45 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Gardener;
+
+using BaseLib.Utils;
+using Gardener.GardenerCode.Character;
+using Godot;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+
+[Pool(typeof(GardenerCardPool))]
+public class SharpRoots() : GardenerCode.Cards.GardenerCard(
+  1,
+  CardType.Attack,
+  CardRarity.Uncommon,
+  TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[] 
+{ new DamageVar(8m, DamageProps.card) };
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+      ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+      await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+      .WithHitFx("vfx/vfx_attack_slash")
+      .Execute(choiceContext);
+
+        await PowerCmd.Apply<PhotosynthesisPower>(
+            choiceContext,
+            base.Owner.Creature,
+            base.DynamicVars["PhotosynthesisPower"].BaseValue,
+            base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+      DynamicVars.Damage.UpgradeValueBy(4m);
+    }
+}
+
