4fce74e874f3e74a1f25a86f08a06b5ab0aa7329
Mgns <you@example.com>
2026-05-29 17:43:21 -0500

    fertilizer, last drop, life cocoon

--- GardenerCode/Cards/LastDrop.cs
+++ GardenerCode/Cards/LastDrop.cs
@@ -11,21 +11,50 @@ using BaseLib.Utils;
 using Gardener.GardenerCode.Character;
 using Gardener.GardenerCode.Systems;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models;
+
+using MegaCrit.Sts2.Core.CardSelection;
+
 
 [Pool(typeof(GardenerCardPool))]
 public class LastDrop() : GardenerCode.Cards.GardenerCard(
-  0,
+  2,
   CardType.Skill,
-  CardRarity.Common,
+  CardRarity.Uncommon,
   TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
     {
-        new IntVar("Nutrient", 2),
+        new IntVar("Nutrient", 8),
+        new RepeatVar(3)
     };
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
+        CardModel? cardModel = (await CardSelectCmd.FromHand(
+            prefs: new CardSelectorPrefs(base.SelectionScreenPrompt, 1),
+            context: choiceContext,
+            player: base.Owner,
+            filter: (CardModel c) => c.DynamicVars["Nutrient"] != null,
+            source: this
+            )
+            ).FirstOrDefault();
+
+        for (int i = 0; i < base.DynamicVars.Repeat.BaseValue; i++)
+        {
+            if (
+                cardModel == null
+                || !cardModel.IsInCombat
+            ) break;
+
+            await CardCmd.AutoPlay(choiceContext, cardModel, null);
+        }
+
+        while (cardModel?.IsInCombat ?? false)
+        {
+            await GardenerCmd.ConsumeNutrient(cardModel);
+        }
+
         await GardenerCmd.ConsumeNutrient(this);
     }
 
--- GardenerCode/Cards/LifeCocoon.cs
+++ GardenerCode/Cards/LifeCocoon.cs
@@ -11,21 +11,42 @@ using BaseLib.Utils;
 using Gardener.GardenerCode.Character;
 using Gardener.GardenerCode.Systems;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Rooms;
 
 [Pool(typeof(GardenerCardPool))]
 public class LifeCocoon() : GardenerCode.Cards.GardenerCard(
-  2,
+  1,
   CardType.Power,
   CardRarity.Rare,
   TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
     {
+        new IntVar("RelicVar", 2)
+    };
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => new CardKeyword[]
+    {
+        CardKeyword.Exhaust
     };
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+    }
+
+    public async Task OnDepletion()
+    {
+        AbstractRoom currentRoom = base.CombatState.RunState.CurrentRoom;
+        if (currentRoom is CombatRoom combatRoom)
+        {
+            for (int i = 0; i < DynamicVars["RelicVar"].BaseValue; i++)
+            combatRoom.AddExtraReward(
+                base.Owner,
+                new RelicReward(base.Owner)
+                );
+        }
     }
 
     protected override void OnUpgrade()
