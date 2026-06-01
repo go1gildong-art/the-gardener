c3bde8ca8f53b4991963ea0c654a7a243243d0b5
Mgns <you@example.com>
2026-06-01 18:55:10 -0500

    add 2 base cards, Nature's Retreat and Weeding

---
+++ GardenerCode/Cards/NatureRetreat.cs
@@ -0,0 +1,42 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Gardener;
+
+using BaseLib.Utils;
+using Gardener.GardenerCode.Character;
+using Gardener.GardenerCode.Powers;
+using Gardener.GardenerCode.Systems;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+[Pool(typeof(GardenerCardPool))]
+public class NatureRetreat() : GardenerCode.Cards.GardenerCard(
+  1,
+  CardType.Skill,
+  CardRarity.Common,
+  TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new IntVar("Nutrient", 12),
+        new BlockVar(4m, BlockProps.card),
+        new PowerVar<NatureRetreatPower>(1m)
+    };
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await CreatureCmd.GainBlock(base.Owner.Creature, base.DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<NatureRetreatPower>(choiceContext, base.Owner.Creature, base.DynamicVars["NatureRetreatPower"].BaseValue, base.Owner.Creature, this);
+        await GardenerCmd.ConsumeNutrient(this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
+}
---
+++ GardenerCode/Cards/Weeding.cs
@@ -0,0 +1,47 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Gardener;
+
+using BaseLib.Utils;
+using Gardener.GardenerCode.Character;
+using Gardener.GardenerCode.Systems;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models;
+
+[Pool(typeof(GardenerCardPool))]
+public class Weeding() : GardenerCode.Cards.GardenerCard(
+  0,
+  CardType.Attack,
+  CardRarity.Common,
+  TargetType.AnyEnemy)
+{
+    protected override bool HasEnergyCostX => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new DamageVar(7m, DamageProps.card),
+    };
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        int cost = ResolveEnergyXValue();
+
+        for (int i = 0; i < cost; i++)
+        {
+            await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+                .WithHitFx("vfx/vfx_attack_slash")
+                .Execute(choiceContext);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
