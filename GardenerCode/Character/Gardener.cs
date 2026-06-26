678262c2b804031d050ff820be9e0f8560aa6cba
Mgns <you@example.com>
2026-06-25 22:03:50 -0500

    success adding stickman to gardener's place

--- GardenerCode/Character/Gardener.cs
+++ GardenerCode/Character/Gardener.cs
@@ -58,20 +58,20 @@ public class Gardener : PlaceholderCharacterModel
             return icon;
         }
     }
-    public override string CustomVisualPath => SceneHelper.GetScenePath("creature_visuals/" + PlaceholderID);
-    public override string CustomTrailPath => SceneHelper.GetScenePath("vfx/card_trail_" + PlaceholderID);
-    public override string? CustomMapMarkerPath => ImageHelper.GetImagePath("packed/map/icons/map_marker_" + PlaceholderID + ".png");
-    public override string CustomIconPath => SceneHelper.GetScenePath("ui/character_icons/" + PlaceholderID + "_icon");
-    public override string? CustomIconTexturePath => ImageHelper.GetImagePath("ui/top_panel/character_icon_" + PlaceholderID + ".png");
-    public override string CustomEnergyCounterPath => SceneHelper.GetScenePath("combat/energy_counters/" + PlaceholderID + "_energy_counter");
-    public override string CustomRestSiteAnimPath => SceneHelper.GetScenePath("rest_site/characters/" + PlaceholderID + "_rest_site");
-    public override string CustomMerchantAnimPath => SceneHelper.GetScenePath("merchant/characters/" + PlaceholderID + "_merchant");
-    public override string CustomArmPointingTexturePath => ImageHelper.GetImagePath("ui / hands / " + PlaceholderID + "_arm_point.png");
-    public override string CustomArmRockTexturePath => ImageHelper.GetImagePath("ui / hands / " + PlaceholderID + "_arm_rock.png");
-    public override string CustomArmPaperTexturePath => ImageHelper.GetImagePath("ui/hands/" + PlaceholderID + "_arm_paper.png");
-    public override string CustomArmScissorsTexturePath => ImageHelper.GetImagePath("ui/hands/" + PlaceholderID + "_arm_scissors.png");
-    public override string CustomCharacterSelectBg => SceneHelper.GetScenePath("screens / char_select / char_select_bg_" + PlaceholderID);
-    public override string CustomCharacterSelectTransitionPath => "res://materials/transitions/" + PlaceholderID + "_transition_mat.tres";
-    public override string? CustomCharacterSelectIconPath => ImageHelper.GetImagePath("packed/character_select/char_select_" + PlaceholderID + ".png");
-    public override string? CustomCharacterSelectLockedIconPath => ImageHelper.GetImagePath("packed/character_select/char_select_" + PlaceholderID + "_locked.png");
+    public override string CustomVisualPath => "res://Gardener/scenes/gardener.tscn";
+    // public override string CustomTrailPath => SceneHelper.GetScenePath("vfx/card_trail_" + PlaceholderID);
+    // public override string? CustomMapMarkerPath => ImageHelper.GetImagePath("packed/map/icons/map_marker_" + PlaceholderID + ".png");
+    // public override string CustomIconPath => SceneHelper.GetScenePath("ui/character_icons/" + PlaceholderID + "_icon");
+    // public override string? CustomIconTexturePath => ImageHelper.GetImagePath("ui/top_panel/character_icon_" + PlaceholderID + ".png");
+    // public override string CustomEnergyCounterPath => SceneHelper.GetScenePath("combat/energy_counters/" + PlaceholderID + "_energy_counter");
+    // public override string CustomRestSiteAnimPath => SceneHelper.GetScenePath("rest_site/characters/" + PlaceholderID + "_rest_site");
+    // public override string CustomMerchantAnimPath => SceneHelper.GetScenePath("merchant/characters/" + PlaceholderID + "_merchant");
+    // public override string CustomArmPointingTexturePath => ImageHelper.GetImagePath("ui / hands / " + PlaceholderID + "_arm_point.png");
+    // public override string CustomArmRockTexturePath => ImageHelper.GetImagePath("ui / hands / " + PlaceholderID + "_arm_rock.png");
+    // public override string CustomArmPaperTexturePath => ImageHelper.GetImagePath("ui/hands/" + PlaceholderID + "_arm_paper.png");
+    // public override string CustomArmScissorsTexturePath => ImageHelper.GetImagePath("ui/hands/" + PlaceholderID + "_arm_scissors.png");
+    // public override string CustomCharacterSelectBg => SceneHelper.GetScenePath("screens / char_select / char_select_bg_" + PlaceholderID);
+    // public override string CustomCharacterSelectTransitionPath => "res://materials/transitions/" + PlaceholderID + "_transition_mat.tres";
+    // public override string? CustomCharacterSelectIconPath => ImageHelper.GetImagePath("packed/character_select/char_select_" + PlaceholderID + ".png");
+    // public override string? CustomCharacterSelectLockedIconPath => ImageHelper.GetImagePath("packed/character_select/char_select_" + PlaceholderID + "_locked.png");
 }
